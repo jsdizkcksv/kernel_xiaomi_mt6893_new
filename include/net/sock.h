750a0961919112495ebaa0d1c13b1e26b9649f0f
Christoph Hellwig <hch@lst.de>
2020-05-28 07:12:13 +0200

    UPSTREAM: net: add sock_bindtoindex
    Add a helper to directly set the SO_BINDTOIFINDEX sockopt from kernel
    space without going through a fake uaccess.
    
    Change-Id: Idb5a895062810cb89b3b7dca8a774b8b2c6161d5
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2544,4 +2544,5 @@ static inline int sk_get_rmem0(const struct sock *sk, const struct proto *proto)
 	return *proto->sysctl_rmem;
 }
 
+int sock_bindtoindex(struct sock *sk, int ifindex);
 #endif	/* _SOCK_H */

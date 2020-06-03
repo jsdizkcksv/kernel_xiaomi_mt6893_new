e9f4d200d09ff4949f882358b6f1d137f466a4c7
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-06-03 21:03:47 +0200

    UPSTREAM: bpf: Fix unused-var without NETDEVICES
    A recent commit added new variables only used if CONFIG_NETDEVICES is
    set. A simple fix would be to only declare these variables if the same
    condition is valid but Alexei suggested an even simpler solution:
    
        since CONFIG_NETDEVICES doesn't change anything in .h I think the
        best is to remove #ifdef CONFIG_NETDEVICES from net/core/filter.c
        and rely on sock_bindtoindex() returning ENOPROTOOPT in the extreme
        case of oddly configured kernels.
    
    Fixes: 70c58997c1e8 ("bpf: Allow SO_BINDTODEVICE opt in bpf_setsockopt")
    Suggested-by: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Change-Id: Ic5d516c10deacc69469599a92533d182d9c402d3
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20200603190347.2310320-1-matthieu.baerts@tessares.net

--- net/core/filter.c
+++ net/core/filter.c
@@ -4408,8 +4408,6 @@ static int _bpf_setsockopt(struct sock *sk, int level, int optname,
 			}
 			break;
 		case SO_BINDTODEVICE:
-			ret = -ENOPROTOOPT;
-#ifdef CONFIG_NETDEVICES
 			optlen = min_t(long, optlen, IFNAMSIZ - 1);
 			strncpy(devname, optval, optlen);
 			devname[optlen] = 0;
@@ -4428,7 +4426,6 @@ static int _bpf_setsockopt(struct sock *sk, int level, int optname,
 				dev_put(dev);
 			}
 			ret = sock_bindtoindex(sk, ifindex, false);
-#endif
 			break;
 		default:
 			ret = -EINVAL;

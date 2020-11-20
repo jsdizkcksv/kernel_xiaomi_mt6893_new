b86b40ae8e6f6e0740e12ba1283150fb8b156f93
Eric W. Biederman <ebiederm@xmission.com>
2020-11-20 17:14:25 -0600

    UPSTREAM: file: Factor files_lookup_fd_locked out of fcheck_files
    To make it easy to tell where files->file_lock protection is being
    used when looking up a file create files_lookup_fd_locked.  Only allow
    this function to be called with the file_lock held.
    
    Update the callers of fcheck and fcheck_files that are called with the
    files->file_lock held to call files_lookup_fd_locked instead.
    
    Hopefully this makes it easier to quickly understand what is going on.
    
    The need for better names became apparent in the last round of
    discussion of this set of changes[1].
    
    [1] https://lkml.kernel.org/r/CAHk-=wj8BQbgJFLa+J0e=iT-1qpmCRTbPAJ8gd6MJQ=kbRPqyQ@mail.gmail.com
    Link: https://lkml.kernel.org/r/20201120231441.29911-8-ebiederm@xmission.com
    Change-Id: I8bd0dfc2db9756bdf3764d526fc8e6c292b1972f
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- fs/file.c
+++ fs/file.c
@@ -1064,7 +1064,7 @@ SYSCALL_DEFINE3(dup3, unsigned int, oldfd, unsigned int, newfd, int, flags)
 
 	spin_lock(&files->file_lock);
 	err = expand_files(files, newfd);
-	file = fcheck(oldfd);
+	file = files_lookup_fd_locked(files, oldfd);
 	if (unlikely(!file))
 		goto Ebadf;
 	if (unlikely(err < 0)) {
